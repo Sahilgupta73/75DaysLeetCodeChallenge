e8efccf48365af5347721ebab15c741fda1a08b7
Sahilgupta73 <sahilgupta65338@gmail.com>
2026-06-12 10:17:06 +0530

    Time: 27 ms (92.93%), Space: 21.8 MB (57.28%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,17 +1,32 @@
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+#         count = 0
+#         prefix_sum = 0
+#         mp = {0: 1}
+
+#         for num in nums:
+#             prefix_sum += num
+
+#             need = prefix_sum - k
+
+#             if need in mp:
+#                 count += mp[need]
+
+#             mp[prefix_sum] = mp.get(prefix_sum, 0) + 1
+
+#         return count
+
+from typing import List
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0
-        prefix_sum = 0
         mp = {0: 1}
+        prefix = 0
+        ans = 0
 
-        for num in nums:
-            prefix_sum += num
+        for x in nums:
+            prefix += x
+            ans += mp.get(prefix - k, 0)
+            mp[prefix] = mp.get(prefix, 0) + 1
 
-            need = prefix_sum - k
-
-            if need in mp:
-                count += mp[need]
-
-            mp[prefix_sum] = mp.get(prefix_sum, 0) + 1
-
-        return count
+        return ans
