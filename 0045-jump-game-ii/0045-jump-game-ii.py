cf6094760413d6ef24754094fa9fb950c5de16c6
Sahilgupta73 <sahilgupta65338@gmail.com>
2026-06-25 11:14:49 +0530

    Time: 4415 ms (5.93%), Space: 22.6 MB (5.27%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,24 @@
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        self.n = len(nums)
+        self.dp = [-1] * self.n
+        return self.find(nums, 0)
+
+    def find(self, nums: List[int], i: int) -> int:
+        if i == self.n - 1:
+            return 0
+
+        if self.dp[i] != -1:
+            return self.dp[i]
+
+        mini = float('inf')
+
+        for j in range(i + 1, min(self.n - 1, i + nums[i]) + 1):
+            curr = self.find(nums, j)
+            mini = min(curr + 1, mini)
+
+        self.dp[i] = mini
+        return self.dp[i]
+        
