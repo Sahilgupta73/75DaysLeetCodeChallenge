415b00b835d3554df7f0c4134db92702f52d6b30
Sahilgupta73 <sahilgupta65338@gmail.com>
2026-06-25 11:19:22 +0530

    Time: 4408 ms (5.94%), Space: 22.8 MB (5.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,4 +1,4 @@
-
+from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
@@ -13,7 +13,7 @@ class Solution:
         if self.dp[i] != -1:
             return self.dp[i]
 
-        mini = float('inf')
+        mini = float("inf")
 
         for j in range(i + 1, min(self.n - 1, i + nums[i]) + 1):
             curr = self.find(nums, j)
@@ -21,4 +21,11 @@ class Solution:
 
         self.dp[i] = mini
         return self.dp[i]
-        
+
+
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         maxi = 0,curr = 0,count = 0
+
+#         for i in range(0,len(nums)-1):
+#             maxi = maxi
