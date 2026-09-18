9fa699cd855ad24ab552cb4d9a793341d5f069ca
Sahilgupta73 <sahilgupta65338@gmail.com>
2026-09-18 23:43:26 +0530

    Time: 47 ms (91.76%), Space: 29 MB (61.85%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,10 +1,21 @@
-class Solution(object):
-    def findMaxAverage(self, nums, k):
-        window_sum = sum(nums[:k])
-        max_sum = window_sum
+class Solution:
+    def findMaxAverage(self, nums: list[int], k: int) -> float:
+        # sum_word = sum(nums[:k])
+
+        # max_word = sum_word
+
+        # for i in range(k, len(nums)):
+        #     max_word = max_word + nums[i] - nums[i-k]
+        #     max_word = max(max_word, sum_word)
+        
+        # return max_word/k
+        
+        sum_word = sum(nums[:k])
+
+        max_word = sum_word
 
         for i in range(k, len(nums)):
-            window_sum = window_sum + nums[i] - nums[i-k]
-            max_sum = max(max_sum, window_sum)
+            sum_word = sum_word + nums[i] - nums[i-k]
+            max_word = max(max_word, sum_word)
 
-        return max_sum / float(k)
+        return max_word / k
